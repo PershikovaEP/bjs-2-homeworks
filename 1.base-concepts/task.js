650775420da1941ad50c961cf8c3e87b2d820c20
Elena Pershikova <zem524@mail.ru>
2022-10-02 01:03:51 +0300

    done task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,48 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  // a * x**2 + b * x + c = 0;  уравнение
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
+
+  if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+    arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+  } else if (discriminant === 0) {
+      arr.push(-b / (2 * a));
+    }
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
+  if (Number.isNaN(Number(percent))) {
+    return totalAmount = 'Параметр "Процентная ставка" содержит неправильное значение ' + '\"' + percent + '\"';
+  } else {
+    percent = Number(percent);
+  }
 
+    if (Number.isNaN(Number(contribution))) {
+    return totalAmount = 'Параметр "Начальный взнос" содержит неправильное значение ' + '\"' + contribution + '\"';
+  } else {
+    contribution = Number(contribution);
+  }
+
+    if (Number.isNaN(Number(amount))) {
+    return totalAmount = 'Параметр "Общая стоимость" содержит неправильное значение ' + '\"' + amount + '\"';
+  } else {
+    amount = Number(amount);
+  }
+
+  let loanBody = amount - contribution;
+  date = new Date(date);
+  let currentDate = new Date;  
+  let month = date.getMonth() - currentDate.getMonth() + 
+  (12 * (date.getFullYear() - currentDate.getFullYear()));
+  let interestRate = percent / 12 / 100;
+  let monthlyPayment = loanBody * (interestRate + (interestRate / (((1 + interestRate) ** month) - 1)));
+  totalAmount = Number((monthlyPayment * month).toFixed(2));
+  console.log(totalAmount);
   return totalAmount;
 }
