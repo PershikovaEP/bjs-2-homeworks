e18f96df81ba29342c3482675a15fe4e4ec7001a
Elena Pershikova <zem524@mail.ru>
2022-10-16 00:36:47 +0300

    done task4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,43 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
+
+let stud1 = new Student("Oleg", "male", 26);
+let stud2 = new Student("Olga", "female", 28);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark]; 
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...mark) {
+  if(this.marks === undefined){ 
+    this.marks = mark; 
+    } else {
+      this.marks.push(...mark);
+    }
+}
+
+Student.prototype.getAverage = function () {
+  let arrElements = Object.assign(this.marks);
+  let sum = arrElements.reduce((acc, item) => { return acc + item; }, 0);
+  let average = sum / this.marks.length; 
+  return average; 
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;  
+  this.excluded = reason;
+}
+
+
